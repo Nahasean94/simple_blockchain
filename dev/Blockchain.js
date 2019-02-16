b5441bbee7f3f5903994f2485a0f1a25dcf68145
Nahashon Njenga <nahaseannjenga@gmail.com>
2019-02-16 14:38:35 +0300

    Initial commit

---
+++ dev/Blockchain.js
@@ -0,0 +1,11 @@
+  class Blockchain{
+    constructor(){
+        this.state={
+            chain:[],
+            newTransactions:[]
+        }
+    }
+    createNewBlock(nonce,previousBlockHash,hash){
+
+    }
+  }
